d706d4a62824bf6f577c30d44b722a34542f2e12
JP Juliao <juan.juliao@projekt202.com>
2022-08-01 16:40:40 -0500

    setup articles cell in home page

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,10 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
+
+<script>
+
+</script>
 
 const HomePage = () => {
   return (
@@ -14,6 +19,8 @@ const HomePage = () => {
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
+
+      <ArticlesCell />
     </>
   )
 }
