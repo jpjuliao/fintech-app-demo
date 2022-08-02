624304fa7a612ae4741896fb41577156f1df26e2
JP Juliao <juan.juliao@projekt202.com>
2022-08-02 01:32:10 -0500

    add alphavantage request

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import ArticlesCell from 'src/components/ArticlesCell'
-
-<script>
-
-</script>
+import { useEffect, useState } from 'react'
 
 const HomePage = () => {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=Q5WZNTK4HUWLQ51T`)
+      .then((response) => response.json())
+      .then((actualData) => console.log(actualData));
+  }, []);
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
